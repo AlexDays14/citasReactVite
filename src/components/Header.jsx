c5959f55f05039ba97a8d7a48b4801f79c8480f1
AlexDays14 <al02954463@tecmilenio.mx>
2022-06-14 13:57:17 -0600

    No fue mucho trabajo pero fue trabajo honesto:)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header(props){
 
     return(
         <>
-        <h1 className="font-black text-5xl text-center mx-auto md:w-2/3">Seguimiento Pacientes <span className="text-indigo-600">Veterinaria</span></h1>
+        <h1 className="font-black text-5xl text-center mx-auto md:w-2/3">Seguimiento Pacientes <span className="text-indigo-600">Veterinaria Con Github</span></h1>
         </>
     )
 }
